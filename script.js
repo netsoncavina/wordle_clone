38744a0870ed1d174afa1c7899e1aeb3a6eaffff
Netson <netsoncavina@gmail.com>
2022-03-19 01:14:00 -0300

    added add/remove letter functions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 import words from "./targetWords.js";
 import dictionary from "./dictionary.js";
 
+const guessGrid = document.querySelector("[data-guess-grid]");
+const WORD_LENGTH = 5;
+const targetWord = words[Math.floor(Math.random() * words.length)];
+console.log(targetWord);
 startInteraction();
 function startInteraction() {
   document.addEventListener("click", handleMouseClick);
@@ -28,7 +32,7 @@ function handleMouseClick(e) {
 }
 
 function handleKeyPress(e) {
-  console.log(e);
+  //   console.log(e);
   if (e.key === "Enter") {
     submitGuess();
     return;
@@ -41,4 +45,35 @@ function handleKeyPress(e) {
     pressKey(e.key);
     return;
   }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile == null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.letter;
+  delete lastTile.dataset.state;
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()];
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Not enough letters");
+    shakeTiles(activeTiles);
+    return;
+  }
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll("[data-state='active']");
 }
